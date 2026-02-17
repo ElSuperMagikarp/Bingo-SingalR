01d524cd7d40d416b61ce11fafea47381de1c3a3
ElSuperMagikarp <uriens2006@gmail.com>
2026-02-17 19:35:26 +0100

    Add project files.

---
+++ BingoClient/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace BingoClient
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
